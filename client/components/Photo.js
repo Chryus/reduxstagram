62d24b357606604f3a09792aa366360cb6baf001
Chris <chris.haack@me.com>
2017-02-11 10:37:38 -0500

    add photo component

---
+++ client/components/Photo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Photo extends React.Component {
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+    return (
+      <figure className="grid-figure">
+        I am a photo!
+      </figure>
+    )
+  }
+}
+
+export default Photo;
--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import Photo from './Photo';
 
-const PhotoGrid = React.createClass({
-  render() {
+class PhotoGrid extends React.Component {
+
+  constructor(props) {
+    super(props);
+  };
+
+  puke() {
     return (
-      <div className='photo-grid'>
-        I'm the photo grid
+      <pre>
+        {JSON.stringify(this.props.posts, null, ' ')}
+      </pre>
+    )
+  }
+
+  render() {
+    //return this.puke(this.props.posts);
+    return (
+      <div className="photo-grid">
+        {this.props.posts.map((post, i) => <Photo />)}
       </div>
     )
   }
-});
+}
 
 export default PhotoGrid;
