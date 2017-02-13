0d93e04d02167baea7c9468c58b0cc4b66a1c6fe
Chris <chris.haack@me.com>
2017-02-13 10:42:01 -0500

    return a copy of the state and increment likes

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,8 +1,15 @@
 const posts = (state=[], action) => {
   console.log(state, action);
+  const i = action.index;
   switch (action.type) {
     case 'INCREMENT_LIKES':
-      return state[action.index].likes + 1;
+      const post = state[i];
+      post.likes + 1;
+      return [
+        ...state.slice(0, i), // everything before index
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i+1) // everything after index
+      ]
     default:
       return state;
   }
