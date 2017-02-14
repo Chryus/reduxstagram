e95539597a504110c579bacd6e90791eed1dbcba
Chris <chris.haack@me.com>
2017-02-14 12:29:32 -0500

    add comments with form

--- client/components/Single.js
+++ client/components/Single.js
@@ -4,14 +4,16 @@ import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const postId = this.props.params.id
     const i = this.props.posts.findIndex((post) => 
-      post.code === this.props.params.id);
+      post.code === postId);
     const post = this.props.posts[i];
+    const comments = this.props.comments[postId] || [];
 
     return (
       <div className='single-photo'>
         <Photo i={i} post={post} {...this.props} />
-        <Comments />
+        <Comments comments={comments} />
       </div>
     )
   }
