41ed328f6263f54849c2ae7d2300c2d822a66aab
Chris <chris.haack@me.com>
2017-02-21 19:10:49 -0500

    import Raven and connect Sentry for v cool debugging

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -8,6 +8,13 @@ import css from './styles/style.styl';
 // components
 import Main from './components/Main'
 
+import Raven from 'raven-js'
+import { sentry_url } from './data/config'
+
+Raven.config(sentry_url).install();
+
+console.log(window.doesNotExist.nope);
+
 // routes
 import routes from './config/routes'
 
